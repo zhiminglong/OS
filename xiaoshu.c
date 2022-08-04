aef893314acc9fdf5371e7448c1d464b074a8ca5
zhiminglong <1919164926@qq.com>
2022-08-04 14:32:17 +0800

    2022-8-4

---
+++ xiaoshu.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: xiaoshu.c
+	> Author: longzhiming
+	> Mail: no 
+	> Created Time: Thu 04 Aug 2022 12:47:18 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	float a = 0.1, b = 0.2;
+	double c = 0.1,d = 0.2;
+	printf("float: 0.1 + 0.2 = %.32f\n",a+b);
+	printf("double: 0.1 + 0.2 = %.32f\n",c+d);
+	return 0;
+}
+
